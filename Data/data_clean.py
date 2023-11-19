28f476ffdba931541e91e5c63c1fa020b6f1e1de
Kalo Qian <kaloqian@nyu.edu>
2023-11-19 18:19:32 -0500

    reconstructed function complete, added default threshold_num to clean function

--- Data/data_clean.py
+++ Data/data_clean.py
@@ -1,12 +1,17 @@
 import pandas as pd
 
 #################################### Data Adjustment ########################################################
-def drop_ticker (intraday_data, threshold_num):
+def drop_ticker (intraday_data, threshold_num=None):
     '''
     intraday_data: pandas dataframe of intraday data
     threshold_num: int number that requires the function to drop tickers with number of data less than the threshold
     return: pandas dataframe of cleaned intraday data
     '''
+    if threshold_num is None:
+        q1 = np.quantile(data_length, 0.25)
+        q3 = np.quantile(data_length, 0.75)
+        iqr = q3-q1
+        threshold_num = q1-(1.5*iqr)
     tmp = intraday_data.groupby("Ticker").count().Open >= threshold_num
     ticker_name = tmp.loc[tmp==True].index
     cleaned_intraday_data = intraday_data.reset_index().set_index('Ticker').loc[ticker_name]
@@ -14,9 +19,49 @@ def drop_ticker (intraday_data, threshold_num):
 
     return cleaned_intraday_data.reset_index().set_index('timestamp')
 
-def reconstruct_dataframe(cleaned_intraday_data):
-    # Reconstruct the tickers with columns less than 4680 back to 4680.
-    return
+def reconstruct_dataframe(cleaned_data):
+    '''
+    cleaned_data: pandas dataframe of cleaned intraday data
+    return: pandas dataframe of reconstructed intraday data, missing value is set to None
+    '''
+    # Reconstruct the tickers with columns less than 4681 back to 4681.
+    ticker_count = cleaned_data.groupby('Ticker').count().Open
+    
+    # Get the full length of data
+    full_length = max(ticker_count)
+    
+    # Find a ticker without missing data and use this dataframe as a sample
+    for i in range(len(ticker_count)):
+        if ticker_count[i]==full_length:
+            sample_ticker = ticker_count.index[i]
+            break        
+    sample = cleaned_data.loc[cleaned_data['Ticker']==sample_ticker].copy()
+    full_timestamps = sample.index.to_numpy()
+    
+    # Go though all tickers and use a expended version to reconstruct
+    reconstructed_data = None
+    for ticker in ticker_count.index:
+        curr = cleaned_data.loc[cleaned_data['Ticker']==ticker].copy()
+        
+        # If this ticker has missing data, reconstruct it
+        if len(curr)!=full_length:
+            missing_data = curr
+            curr = sample.copy()
+            for timestamp in full_timestamps:
+                # check every timestamp, if not missing, replace the sample row with it
+                if timestamp in missing_data.index:
+                    curr.loc[timestamp] = missing_data.loc[timestamp]
+                # If it is missing, set all value of sample to None, set current tickername
+                else:
+                    curr.loc[timestamp,curr.columns[1:]]=None
+                    curr.loc[timestamp,curr.columns[0]] = ticker
+        # add reconstructed data frame to the result
+        if reconstructed_data is not None:
+            reconstructed_data = pd.concat([reconstructed_data,curr])
+        else:
+            reconstructed_data = curr         
+    return reconstructed_data
+
 def impute_missing_data (cleaned_intraday_data):
     # Impute missing data for the reconstructed tickers.
     return
